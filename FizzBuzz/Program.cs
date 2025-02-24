056081e497ca29d0fa12f21e4cfe1a6b849a155e
Suet Kee <suet.kee@softwire.com>
2025-02-24 21:41:15 +0000

    added extra rules, and allow user to set Max Limit

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,28 +6,64 @@ namespace FizzBuzz;
 
 public class FizzBuzz
 {
-    public static void Main()
-    {   Console.WriteLine("Hello World");
-        FizzBuzz.GetFizzBuzzOutput(101);
+    public static void Main(){
+        int limit = GetValidMaxNumber("Please enter the max number limit: ");
+        for (var i=1; i <limit;i++){
+        Console.WriteLine(GetFizzBuzzOutput(i));
+         }
     }
 
-    public static void GetFizzBuzzOutput(int number) {
+    public static int GetValidMaxNumber(string prompt){
+            int result;
+            bool isValid;
 
-            for(var i=1; i<number;i++){
-                if(i%3==0 && i%5==0){
-                    Console.WriteLine("FizzBuzz");
+            do
+            {
+                Console.WriteLine(prompt);
+                string? input = Console.ReadLine();
+                isValid = int.TryParse(input, out result);
+
+                if(!isValid){
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
                 }
-                else if(i%3==0){
-                    Console.WriteLine("Fizz");
-                } else if (i%5==0){
-                    Console.WriteLine("Buzz");
-                } else {
-                    Console.WriteLine(i);
-                }
+            } while(!isValid);
+            
+            return result;
+    }
+    public static string GetFizzBuzzOutput(int number){
+        Dictionary<int, string> rules  = new Dictionary<int, string> {
+            {3,"Fizz"},{13,"Fezz"}, {5,"Buzz"}, {7,"Bang"} 
+        };
+
+        var output = new List<string>{};
+        string result = "";
+
+        foreach (var rule in rules){
+            if(number%rule.Key==0){
+                output.Add(rule.Value);
             }
-    }
-    
+        }
 
+        if(number%11==0){
+            output.Clear();
+            if(number%13==0){
+                output.Add("Fezz");
+            }
+            output.Add("Bong");
+        }
+
+        if(number%17==0){
+            output.Reverse();
+            }
+        if(output.Count==0){
+            result = number.ToString();
+            } else {
+            result = string.Join("",output);
+            };
+
+     
+        return result ; 
+    }
 
 }
 
