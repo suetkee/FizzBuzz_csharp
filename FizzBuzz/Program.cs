3c2c96cd311211ec2468d87968adf8629ec08c85
Suet Kee <suet.kee@softwire.com>
2025-02-24 13:16:43 +0000

    Basic FizzBuzz

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using Microsoft.VisualBasic;
 
 namespace FizzBuzz;
@@ -7,10 +8,25 @@ public class FizzBuzz
 {
     public static void Main()
     {   Console.WriteLine("Hello World");
+        FizzBuzz.GetFizzBuzzOutput(101);
     }
 
+    public static void GetFizzBuzzOutput(int number) {
 
-
+            for(var i=1; i<number;i++){
+                if(i%3==0 && i%5==0){
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i%3==0){
+                    Console.WriteLine("Fizz");
+                } else if (i%5==0){
+                    Console.WriteLine("Buzz");
+                } else {
+                    Console.WriteLine(i);
+                }
+            }
+    }
+    
 
 
 }
