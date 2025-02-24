007c751d9be79989515953b367a0aa20f546b086
Suet Kee <suet.kee@softwire.com>
2025-02-24 12:03:12 +0000

    new file setup

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace FizzBuzz;
+
+public class FizzBuzz
+{
+    public static void Main()
+    {   Console.WriteLine("Hello World");
+    }
+
+
+
+
+
+}
+
+
+
